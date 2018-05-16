b9246618dc1f3f89da2044e03cbec6ee4c5ff977
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-05-16 13:36:41 -0700

    Token Up & Running

--- server/_config/middleware.js
+++ server/_config/middleware.js
@@ -2,10 +2,12 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const passport = require('passport');
 
 module.exports = function(server) {
   server.use(helmet());
   server.use(morgan('dev'));
   server.use(express.json());
   server.use(cors());
+  server.use(passport.initialize());
 };
--- server/auth/authRoutes.js
+++ server/auth/authRoutes.js
@@ -1,14 +1,57 @@
 const router = require('express').Router();
+// const jwt = require('jsonwebtoken');
+// const passport = require('passport');
+// const LocalStrategy = require('passport-local');
 
-const User = require('../users/User');
+// const User = require('../users/User');
+
+// const secret = 'that is what I shared yesterday lol';
+
+// const localStrategy = newLocalStrategy(function(username, password, done) {
+//   User.findOne({username}).then(user => {
+//     if(!user) {
+//       done(null, false);
+//     } else {
+//       user
+//         .validatePassword(password)
+//         .then(isValid => {
+//           const { _id, username } = user;
+//           return done(null, { _id, username });
+//         })
+//         .catch(err => {
+//           return done(err);
+//         });
+//     }
+//   }).catch(err => done(err))
+// });
 
 router.post('/register', function(req, res) {
-  console.log('posting', req.body);
-  User.create(req.body)
+  // console.log('posting', req.body);
+  User.create(req.body) // new User + user.save
     .then(user => {
-      res.status(201).json(user);
+      const token = makeToken(user);
+      res.status(201).json({ user, token });
     })
     .catch(err => res.status(500).json(err));
 });
+
+// router.post('/login', authenticate, (req, res) => {
+
+// });
+
+// function makeToken(user) {
+//   // build that token
+//   const timestamp = new Date().getTime();
+//   const payload = {
+//     sub: user._id,
+//     iat: timestamp,
+//     username: user.username,
+//   };
+//   const options = {
+//     expiresIn: '24h',
+//   };
+
+//   return jwt.sign(payload, secret, options)
+// }
 
 module.exports = router;
